8a00a4df46c8fa04d7071ec9f8645f1e32b5311e
Rushikesh <30998401+rk-7@users.noreply.github.com>
2020-09-18 17:15:08 +0530

    Scatter plot implementation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ScatterPlotComponent } from './scatter-plot/scatter-plot.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'scatter-plot', pathMatch: 'full' },
+  { path: 'scatter-plot', component: ScatterPlotComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
